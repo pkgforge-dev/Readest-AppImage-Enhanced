d836c615b931562f3dc633824edc01ced73f3d67
Link4Electronics <link4electronics@gmail.com>
2026-08-30 03:16:21 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,9 +10,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/256x256@2/apps/readest.png
 export DESKTOP=/usr/share/applications/com.bilingify.readest.desktop
-export DEPLOY_OPENGL=1
-export DEPLOY_GTK=1
-export GTK_DIR=gtk-3.0
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/readest
